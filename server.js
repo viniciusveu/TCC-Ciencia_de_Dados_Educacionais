95ea2ec7a8c83f6f0e4c0ff4bc0caf28f00e8468
Vinicius Vedovotto <vinicius.vedovotto@pampa.compasso>
2021-08-19 00:51:43 -0300

    Implementando o portal de CDE

---
+++ server.js
@@ -0,0 +1,22 @@
+const Express = require('express');
+
+const app = Express();
+
+app.use(Express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/index', (req, res) => {
+    res.render('index');
+});
+
+app.get('/classificacao', (req, res) => {
+    res.render('classificacao');
+});
+
+app.get('/regressao', (req, res) => {
+    res.render('regressao');
+});
+
+app.listen(3000, () => {
+    console.log('app listen on port 3000');
+});
