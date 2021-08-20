ee2dc85eb704bcbd50a5312e922be7c2f1e82a6b
Vinicius Vedovotto <vinicius.vedovotto@pampa.compasso>
2021-08-20 20:17:56 -0300

    Adicionando cors

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-//Creanting server
 const Express = require('express');
+const cors = require('cors');
+
+//Creanting server
 const app = Express();
 
 //Config middlewares and views engine
 app.use(Express.static('public'));
+app.use(cors);
 app.set('view engine', 'ejs');
+
+
 
 //Routes
 app.get('/index', (req, res) => res.render('index'));
